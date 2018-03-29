608ea2a3ce41109e6fde5498c9fe1e971a3c6eef
Sam Scherer <swiimii962@gmail.com>
2018-03-29 16:18:34 -0400

    Added old PomegranageBotCode progress. Probably won't be used, but keeping everything up-to-date

---
+++ PomegranateBotCode/PomegranateBotCode/MyBot.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PomegranateBotCode
+{
+    class MyBot
+    {
+        
+
+        public MyBot()
+        {
+            
+        }
+            
+
+
+    }
+}
---
+++ PomegranateBotCode/PomegranateBotCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+
+namespace PomegranateBotCode
+{
+    class Program
+    {
+        MyBot bot = new MyBot();
+
+    }
+}
